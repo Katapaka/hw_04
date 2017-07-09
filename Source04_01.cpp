52479d3acaba9b2e734f327c7b56d945ffd16c95
Katapaka <nuisho0@gmail.com>
2017-07-09 09:10:17 +0300

    some changes

--- Source04_01.cpp
+++ Source04_01.cpp
@@ -1,11 +1,26 @@
 #include<iostream>
 using namespace std;
 
+long long Exponent(int n, int e)
+{
+	long long exp = n;
+	for (int i = 1; i < e; i++)
+		exp *= n;
+	return exp;
+}
+
 int main()
 {
 	//Написать функцию, которая принимает два параметра : 
 	//основание степени и показатель степени, и вычисляет степень числа на основе полученных данных
-	
+	setlocale(LC_ALL, "RUS");
+	int num, exp;
+
+	cout << "Введіть число ";
+	cin >> num;
+	cout << "Введіть степінь ";
+	cin >> exp;
+	cout << num << " в " << exp << " степені дорівнює " << Exponent(num, exp) << endl;
 
 	system("pause");
 	return 0;
